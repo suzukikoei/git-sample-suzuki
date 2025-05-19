c3fd025a8341161aef837e95e036ea03ada99d59
suzukikoei <koeich6@gmail.com>
2025-05-19 13:31:06 +0900

    carクラスの中身実装

--- src/main/java/com/example/domain/Car.java
+++ src/main/java/com/example/domain/Car.java
@@ -1,5 +1,22 @@
 package com.example.domain;
 
 public class Car {
-    
+    private Integer speed;
+    private String name;
+
+    public Integer getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Integer speed) {
+        this.speed = speed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
