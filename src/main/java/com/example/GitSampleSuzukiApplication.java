93c5f539e2d79679d046b0d20b2831efe8cbe729
suzukikoei <koeich6@gmail.com>
2025-05-19 12:03:35 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleSuzukiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleSuzukiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleSuzukiApplication.class, args);
+	}
+
+}
